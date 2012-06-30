bdfd3fd068bdbb2683403aa8763b31cd3dcdcc24
Mohammed El Batya <mohammed@elbatya.de>
2012-06-30 21:28:02 +0200

    moved shutdown to onDestroy

--- cling-mediaserver/src/de/andforge/cling/mediaserver/MainActivity.java
+++ cling-mediaserver/src/de/andforge/cling/mediaserver/MainActivity.java
@@ -8,7 +8,11 @@ import org.teleal.cling.android.AndroidUpnpServiceConfiguration;
 import org.teleal.cling.binding.LocalServiceBindingException;
 import org.teleal.cling.model.ValidationError;
 import org.teleal.cling.model.ValidationException;
+import org.teleal.cling.model.meta.LocalDevice;
+import org.teleal.cling.model.meta.RemoteDevice;
 import org.teleal.cling.registry.RegistrationException;
+import org.teleal.cling.registry.Registry;
+import org.teleal.cling.registry.RegistryListener;
 
 import android.app.Activity;
 import android.net.wifi.WifiManager;
@@ -18,22 +22,17 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private UpnpService upnpService;
 
-        System.out.println("Starting Cling UpnpService ...");
-//		System.setProperty("org.teleal.cling.network.useInterfaces", "en1");
-		WifiManager wifiManager = (WifiManager)this.getSystemService(WIFI_SERVICE);
-		final UpnpService upnpService = new UpnpServiceImpl(new AndroidUpnpServiceConfiguration(wifiManager));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-		Runtime.getRuntime().addShutdownHook(new Thread() {
-			@Override
-			public void run() {
-				upnpService.shutdown();
-			}
-		});
+		System.out.println("Starting Cling UpnpService ...");
+		// System.setProperty("org.teleal.cling.network.useInterfaces", "en1");
+		WifiManager wifiManager = (WifiManager) this.getSystemService(WIFI_SERVICE);
+		upnpService = new UpnpServiceImpl(new AndroidUpnpServiceConfiguration(wifiManager));
 
 		// Add the bound local device to the registry
 		try {
@@ -55,18 +54,75 @@ public class MainActivity extends Activity {
 			e.printStackTrace();
 		}
 
-    }
+	}
 
-    @Override
-    protected void onDestroy() {
-    	super.onDestroy();
-    };
+	@Override
+	protected void onDestroy() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		if (isFinishing()) {
+			new Thread() {
+				@Override
+				public void run() {
+					upnpService.shutdown();
+				}
+			}.start();
+		}
+		super.onDestroy();
+	};
 
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	private class A implements RegistryListener {
+
+		public void afterShutdown() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void beforeShutdown(Registry pArg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void localDeviceAdded(Registry pArg0, LocalDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void localDeviceRemoved(Registry pArg0, LocalDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void remoteDeviceAdded(Registry pArg0, RemoteDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void remoteDeviceDiscoveryFailed(Registry pArg0, RemoteDevice pArg1, Exception pArg2) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void remoteDeviceDiscoveryStarted(Registry pArg0, RemoteDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void remoteDeviceRemoved(Registry pArg0, RemoteDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void remoteDeviceUpdated(Registry pArg0, RemoteDevice pArg1) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
 }
